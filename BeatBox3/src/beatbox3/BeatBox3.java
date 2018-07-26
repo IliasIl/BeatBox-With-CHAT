34be35a28a503140f55883ed0b11fa1eafc02783
Ilias <cossin90@gmail.com>
2018-07-26 16:03:41 +0300

    Создание GUI для BeatBox

---
+++ BeatBox3/src/beatbox3/BeatBox3.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beatbox3;
+
+import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import javax.sound.midi.*;
+import java.util.*;
+import java.awt.event.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.event.*;
+
+public class BeatBox3 {
+
+    JFrame theFrame;
+    JPanel mainPanel;
+    JList incomingList;
+    JTextField userMessage;
+    ArrayList<JCheckBox> checkBoxList;
+    int nextNum;
+    Vector<String> listVector = new Vector<String>();
+    String userName;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    HashMap<String, boolean[]> otherSeqsMap = new HashMap<String, boolean[]>();
+    Sequencer sequencer;
+    Sequence sequence;
+    Sequence mySequence = null;
+    Track track;
+
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat",
+        "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bingo",
+        "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom",
+        "High Agogo", "Open Hi Conga"};
+
+    int[] instruments = {35, 42, 46,
+        38, 49, 39, 50, 60,
+        70, 72, 64, 56, 58, 47,
+        67, 63};
+
+    public static void main(String[] args) {
+
+//new BeatBox3().StartUp(args[0]);
+        new BeatBox3().buildGui();
+    }
+
+    public void StartUp(String name) {
+        userName = name;
+        try {
+            Socket sock = new Socket("127.0.0.1", 4242);
+            out = new ObjectOutputStream(sock.getOutputStream());
+            in = new ObjectInputStream(sock.getInputStream());
+
+            // Thread remote= new Thread(new RemoteReader());
+            //   remote.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        // setUpMidi();
+        buildGui();
+
+    }
+
+    public void buildGui() {
+        theFrame = new JFrame("Cyber BeatBox");
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        checkBoxList = new ArrayList<JCheckBox>();
+
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+        JButton start = new JButton("START");
+        //  start.addActionListener(new MyStartListener);
+        buttonBox.add(start);
+
+        JButton stop = new JButton("STOP");
+        //start.addActionListener(new MyStopListener);
+        buttonBox.add(stop);
+
+        JButton upTempo = new JButton("TEMPO UP");
+        //  start.addActionListener(new MyUpTempoListener);
+        buttonBox.add(upTempo);
+
+        JButton downTempo = new JButton("TEMPO DOWN");
+        //  start.addActionListener(new MyDownTempoListener);
+        buttonBox.add(downTempo);
+
+        JButton sendIt = new JButton("SEND");
+        //  start.addActionListener(new MySendListener);
+        buttonBox.add(sendIt);
+
+        userMessage = new JTextField();
+        buttonBox.add(userMessage);
+
+        incomingList = new JList();
+        //     incomingList.addListSelectionListener(new MyListSelectionListener());
+        incomingList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane theList = new JScrollPane(incomingList);
+        buttonBox.add(theList);
+        incomingList.setListData(listVector);
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i < 16; i++) {
+            nameBox.add(new Label(instrumentNames[i]));
+        }
+        background.add(BorderLayout.EAST, buttonBox);
+        background.add(BorderLayout.WEST, nameBox);
+
+        theFrame.getContentPane().add(background);
+        GridLayout grid = new GridLayout(16, 16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+
+        mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
+
+        for (int i = 0; i < 256; i++) {
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkBoxList.add(c);
+            mainPanel.add(c);
+        }
+        theFrame.setBounds(50, 50, 300, 300);
+        theFrame.pack();
+        theFrame.setVisible(true);
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainPanel.setBackground(Color.yellow);
+
+    }
+
+    public void setUpMidi() {
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void BuildTrackAndStart() {
+        ArrayList<Integer> trackList;
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new ArrayList<Integer>();
+
+        }
+    }
+
+}
